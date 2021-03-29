bb11afcfe4740b4ac4cbd3f6f69943541535c839
Timo Katila <skinbarber@gmail.com>
2021-03-29 22:10:30 +0300

    Add files via upload

--- src/components/TeamStats/Teams.js
+++ src/components/TeamStats/Teams.js
@@ -6,21 +6,7 @@ import Table from 'react-bootstrap/Table';
 
 const Teams = ({ teamStats }) => {
     console.log(teamStats)
-    // Object.entries(teamStats.tilastotRanking).map(([key, value]) => { //Poistaa englanninkieliset loppuliitteet //varmaan forEachillakin olisi voinut tehdä.
-      
-    //     if (value.length > 3) { 
-    //         teamStats.tilastotRanking[key] = value.slice(0,2)
-         
-    //     }
-
-    //     if (value.length === 3) {
-    //         teamStats.tilastotRanking[key] = value.slice(0,1)
-          
-    //     }
-
-    //     return () => {}
-    // })
-   
+     
     return(
         <Col>
         <h2 className="text-center mt-3">Team stats</h2>
@@ -30,82 +16,82 @@ const Teams = ({ teamStats }) => {
                     <th scope="col" colSpan="12">{teamStats.team}</th>
                 </tr>
                 <tr className="thead-light">
-                    <th colSpan="1">Ottelut:</th>
+                    <th colSpan="1">Games played:</th>
                     <th colSpan="1"> {teamStats.stats.gamesPlayed}</th>
                     <th></th>
-                    <th colSpan="1">Vastustajan laukaukset / ottelu:</th>
+                    <th colSpan="1">Opponent shots per game::</th>
                     <th colSpan="1">{teamStats.stats.shotsAllowed}</th>
                     
                 </tr>
                 <tr className="thead-light">
-                    <th colSpan="1">Voitot:</th>
+                    <th colSpan="1">Wins:</th>
                     <th colSpan="1"> {teamStats.stats.wins}</th>
                     <th></th>
-                    <th colSpan="1">Maalivahtien torjuntaprosentti:</th>
+                    <th colSpan="1">Save percentage:</th>
                     <th colSpan="1">{(teamStats.stats.savePctg * 100).toFixed(2)}%</th>
                 </tr>
                 <tr className="thead-light">
-                    <th colSpan="1">Tasapelit:</th>
+                    <th colSpan="1">Overtime:</th>
                     <th colSpan="1"> {teamStats.stats.ot}</th>
                     <th></th>
-                    <th colSpan="1">Laukauksia / ottelu:</th>
+                    <th colSpan="1">Shots per game:</th>
                     <th colSpan="1">{teamStats.stats.shotsPerGame}</th>
                 </tr>
                 <tr className="thead-light">
-                    <th colSpan="1">Häviöt:</th>
+                    <th colSpan="1">Losses:</th>
                     <th colSpan="1"> {teamStats.stats.losses}</th>
                     <th></th>
-                    <th colSpan="1">Laukausten maalintekoprosentti:</th>
+                    <th colSpan="1">Shooting percentage:</th>
                     <th colSpan="1">{teamStats.stats.shootingPctg}%</th>
                  
                 </tr>
                 <tr className="thead-light">
-                    <th colSpan="1">Pisteet:</th>
+                    <th colSpan="1">Points:</th>
                     <th colSpan="1"> {teamStats.stats.pts}</th>
                     <th></th>
-                    <th colSpan="1">Ylivoimia:</th>
+                    <th colSpan="1">Powerplay opportunities:</th>
                     <th colSpan="1">{teamStats.stats.powerPlayOpportunities} </th>
                 </tr>
                 <tr className="thead-light">
-                    <th colSpan="1">Aloitukset:</th>
+                    <th colSpan="1">Faceoffs:</th>
                     <th colSpan="1">{teamStats.stats.faceOffsTaken}</th>
                     <th></th>
-                    <th colSpan="1">Ylivoima maalit:</th>
+                    <th colSpan="1">Powerplay goals:</th>
                     <th colSpan="1">{teamStats.stats.powerPlayGoals} </th>
                 </tr>
                 <tr className="thead-light">
-                    <th colSpan="1">Aloituksia voitettu:</th>
+                    <th colSpan="1">Faceoffs won:</th>
                     <th colSpan="1">{teamStats.stats.faceOffsWon}</th>
                     <th></th>
-                    <th colSpan="1">Päästetyt ylivoimamaalit:</th>
+                    <th colSpan="1">Powerplay goals against:</th>
                     <th colSpan="1">{teamStats.stats.powerPlayGoalsAgainst} </th>
                 </tr>
                 <tr className="thead-light">
-                    <th colSpan="1">AloituksiaHäviöt:</th>
+                    <th colSpan="1">Faceoff lost:</th>
                     <th colSpan="1">{teamStats.stats.faceOffsLost}</th>
                     <th></th>
-                    <th colSpan="1">Ylivoima %</th>
-                    <th colSpan="1">{teamStats.stats.powerPlayGoalsAgainst}% </th>
+                    <th colSpan="1">Powerplay %</th>
+                    <th colSpan="1">{teamStats.stats.powerPlayPercentage}% </th>
                 </tr>
                 <tr className="thead-light">
-                    <th colSpan="1">Aloitusvoitto %</th>
+                    <th colSpan="1">Faceoff won %</th>
                     <th colSpan="1">{ (teamStats.stats.faceOffsWon / teamStats.stats.faceOffsTaken * 100).toFixed(2)}%</th>
                     <th></th>
-                    <th colSpan="1">Alivoima %</th>
+                    <th colSpan="1">Penalty kill %</th>
                     <th colSpan="1">{teamStats.stats.penaltyKillPercentage}% </th>
                 </tr>
                 <tr className="thead-light">
-                    <th colSpan="1">Aloitustenhäviö %</th>
+                    <th colSpan="1">Faceoffs lost %</th>
                     <th colSpan="1">{ (teamStats.stats.faceOffsLost / teamStats.stats.faceOffsTaken * 100).toFixed(2)}%</th>
                     <th colSpan="10"></th>
                 </tr>
                 <tr className="thead-light">
-                    <th colSpan="1">Maaleja tehty / ottelu</th>
+                    <th colSpan="1">Goals per game:</th>
                     <th colSpan="1">{ teamStats.stats.goalsPerGame}</th>
                     <th colSpan="10"></th>
                 </tr>
                 <tr className="thead-light">
-                    <th colSpan="1">Maaleja päästetty / ottelu</th>
+                    <th colSpan="1">Goals against per game:</th>
                     <th colSpan="1">{ teamStats.stats.goalsAgainstPerGame}</th>
                     <th colSpan="10"></th>
                 </tr>
@@ -113,81 +99,81 @@ const Teams = ({ teamStats }) => {
                 {/* //Ranking */}
 
                 <tr className="text-center">
-                    <th scope="col" colSpan="12">Ranking</th>
+                    <th scope="col" colSpan="12">Overall Ranking</th>
                 </tr>
                 <tr className="thead-light">
-                    <th colSpan="1">Voittoja:</th>
+                    <th colSpan="1">Wins:</th>
                     <th colSpan="1">{ teamStats.statsRanking.wins}</th>
                     <th colSpan=""></th>
-                    <th colSpan="1">Vastustajan laukaukset:</th>
+                    <th colSpan="1">Shots allowed:</th>
                     <th colSpan="1">{teamStats.statsRanking.shotsAllowed} </th>
                 </tr>
                 <tr className="thead-light">
-                    <th colSpan="1">Tasapelit:</th>
+                    <th colSpan="1">Overtime:</th>
                     <th colSpan="1">{ teamStats.statsRanking.ot}</th>
                     <th></th>
-                    <th colSpan="1">Maalivahtien torjuntaprosentti:</th>
+                    <th colSpan="1">Goealies save percentage:</th>
                     <th colSpan="1">{teamStats.statsRanking.savePctRank} </th>
                 </tr>
                 <tr className="thead-light">
-                    <th colSpan="1">Vähiten häviöitä:</th>
+                    <th colSpan="1">Losses:</th>
                     <th colSpan="1">{ teamStats.statsRanking.losses}</th>
                     <th></th>
-                    <th colSpan="1">Laukauksia / ottelu:</th>
+                    <th colSpan="1">Shots per game:</th>
                     <th colSpan="1">{teamStats.statsRanking.shotsPerGame} </th>
                 </tr>
                 <tr className="thead-light">
-                    <th colSpan="1">Pisteitä:</th>
+                    <th colSpan="1">Points:</th>
                     <th colSpan="1">{ teamStats.statsRanking.pts}</th>
                     <th></th>
-                    <th colSpan="1">Laukausten maalintekoprosentti:</th>
+                    <th colSpan="1">Shooting percentage:</th>
                     <th colSpan="1">{teamStats.statsRanking.shootingPctRank} </th>
                 </tr>
                 <tr className="thead-light">
-                    <th colSpan="1">Pisteitä / peli:</th>
+                    <th colSpan="1">Points per game:</th>
                     <th colSpan="1">{ teamStats.statsRanking.ptPctg}</th>
                     <th></th>
-                    <th colSpan="1">Ylivoima:</th>
+                    <th colSpan="1">Powerplay opportunities:</th>
                     <th colSpan="1">{teamStats.statsRanking.powerPlayOpportunities} </th>
                 </tr>
                 <tr className="thead-light">
-                    <th colSpan="1">Aloituksia:</th>
+                    <th colSpan="1">Faceoffs taken:</th>
                     <th colSpan="1">{ teamStats.statsRanking.faceOffsTaken}</th>
                     <th></th>
-                    <th colSpan="1">Ylivoiman maalinteko %</th>
+                    <th colSpan="1">Powerplay goal %</th>
                     <th colSpan="1">{teamStats.statsRanking.powerPlayPercentage} </th>
                 </tr>
                 <tr className="thead-light">
-                    <th colSpan="1">Aloitusvoitot:</th>
+                    <th colSpan="1">Faceoffs won:</th>
                     <th colSpan="1">{ teamStats.statsRanking.faceOffsWon}</th>
                     <th></th>
-                    <th colSpan="1">Ylivoima maalit:</th>
+                    <th colSpan="1">Powerplay goals:</th>
                     <th colSpan="1">{teamStats.statsRanking.powerPlayGoals} </th>
                     
                 </tr>
                 <tr className="thead-light">
-                    <th colSpan="1">Aloitushäviöt:</th>
+                    <th colSpan="1">Faceoffs lost:</th>
                     <th colSpan="1">{ teamStats.statsRanking.faceOffsLost}</th>
                     <th></th>
-                    <th colSpan="1">Ylivoimalla päästetyt maalit:</th>
+                    <th colSpan="1">Powerplay goals against:</th>
                     <th colSpan="1">{teamStats.statsRanking.powerPlayGoalsAgainst} </th>
                 </tr>
                 <tr className="thead-light">
-                    <th colSpan="1">Aloitus voitot %</th>
+                    <th colSpan="1">Face off won %</th>
                     <th colSpan="1">{ teamStats.statsRanking.faceOffWinPercentage}%</th>
                     <th></th>
-                    <th colSpan="1">Jäähyjä:</th>
+                    <th colSpan="1">Penalty kill opportunities:</th>
                     <th colSpan="1">{teamStats.statsRanking.penaltyKillOpportunities} </th>
                 </tr>
                 <tr className="thead-light">
-                <th colSpan="1">Maaleja tehty / ottelu:</th>
+                <th colSpan="1">Goals per game:</th>
                     <th colSpan="1">{ teamStats.statsRanking.goalsPerGame}</th>
                     <th></th>
-                    <th colSpan="1">Alivoimalla päästetyt maalit:</th>
-                    <th colSpan="1">{teamStats.statsRanking.powerPlayGoalsAgainst} </th>
+                    <th colSpan="1">Penalty Kill percentage:</th>
+                    <th colSpan="1">{teamStats.statsRanking.penaltyKillPercentage} </th>
                 </tr>
                 <tr className="thead-light">
-                    <th colSpan="1">Maaleja päästetty / ottelu:</th>
+                    <th colSpan="1">Goals against per game:</th>
                     <th colSpan="1">{ teamStats.statsRanking.goalsAgainstPerGame}</th>
                     <th colSpan="10"></th>
                 </tr>
